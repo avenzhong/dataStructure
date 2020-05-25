9426b93e62caf56c07b7cf7b688b75bd236dc2c1
钟文坚 <avensnowman@gmail.com>
2020-05-26 07:43:02 +0800

    增加判断head==0时队列才为空

--- src/java/queue/ArrayQueue.java
+++ src/java/queue/ArrayQueue.java
@@ -28,8 +28,20 @@ public class ArrayQueue {
 
     //入队
     public boolean enqueue(String item){
-        // 如果tail == n 表示队列已经满了
-        if (tail == n) return false;
+        // 如果tail == n 表示队尾没空间了
+        if (tail == n) {
+            //tail == n && head == 0表示整个队列都占满了
+            if (head == 0){
+                return false;
+            }
+            //数据搬移
+            for (int i = head;i < tail;++i){
+                items[i - head] = items[i];
+            }
+            //搬移完之后更新head和tail
+            tail -= head;
+            head = 0;
+        }
         items[tail] = item;
         ++tail;
         return true;
@@ -47,7 +59,7 @@ public class ArrayQueue {
 
     public void printAll() {
         for (int i = head; i < tail; ++i) {
-            System.out.print(items[i] + " ");
+            System.out.print(items[i] + " " + "i=" + i);
         }
         System.out.println();
     }
@@ -58,6 +70,15 @@ public class ArrayQueue {
         queue.enqueue("2");
         queue.enqueue("3");
         queue.enqueue("4");
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+
+        queue.enqueue("4");
+        queue.enqueue("4");
+        queue.enqueue("4");
+
         queue.enqueue("5");
 
         queue.printAll();
